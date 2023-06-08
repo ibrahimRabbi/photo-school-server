5b1061879d540568b326084e62e40e843d1e5ad8
ibrahim rabbi <ibrahimrabbihere@gmail.com>
2023-06-08 22:07:06 +0600

    selected class get by query parameter implement done

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const user = process.env.MONGO_USER;
 const pass = process.env.MONGO_PASS;
 const uri = `mongodb+srv://${user}:${pass}@cluster0.oqkryfl.mongodb.net/?retryWrites=true&w=majority`;
 
- 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -24,35 +23,37 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    
-      const classCollaction = client.db("school").collection('class');
-      const classSelectCollaction = client.db("school").collection('selectClass');
 
-    
-//all class data get api
-      app.get('/class', async (req, res) => {
-          const data = await classCollaction.find().toArray()
-          res.send(data)
-      })
+    const classCollaction = client.db("school").collection("class");
+    const classSelectCollaction = client.db("school").collection("selectClass");
 
-// class seleceted data post api
-    app.post('/select', async (req, res) => {
-      const data = req.body
-      const result = await classSelectCollaction.insertOne(data)
-      res.send(result)
-    })
-      
-      
-      
-      
-      
-    
+    //all class data get api
+    app.get("/class", async (req, res) => {
+      const data = await classCollaction.find().toArray();
+      res.send(data);
+    });
+
+    // class seleceted data post api
+    app.post("/select", async (req, res) => {
+      const data = req.body;
+      const result = await classSelectCollaction.insertOne(data);
+      res.send(result);
+    });
+
+    //class selected data get using email api
+    app.get("/select", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { userEmail: req.query.email };
+      }
+
+      const data = await classSelectCollaction.find(query).toArray();
+      res.send(data);
+    });
   } finally {
-    
   }
 }
 run().catch(console.dir);
-
 
 app.get("/", (req, res) => {
   res.send("server is running");
